1a693fcb103313bc0c9b03c2bd9ec79fe9d7f69b
Cuong Ngo <cuong.ngo@ascendcorp.com>
2018-10-18 18:03:50 +0700

    Use template to render web view:

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Cuong Ngo',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'October 17, 2018'
+    },
+    {
+        'author': 'Mark Tuan',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 17, 2018'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
