1dac0db6c58d79245ccd78b52f976e3b7a84eba0
Cuong Ngo <cuong.ngo@ascendcorp.com>
2018-10-18 20:46:39 +0700

    Register new users app with main project

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'Cuong Ngo',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'October 17, 2018'
-    },
-    {
-        'author': 'Mark Tuan',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'October 17, 2018'
-    }
-]
-
-
 def home(request):
     context = {
         'posts': Post.objects.all()
